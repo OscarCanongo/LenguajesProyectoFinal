075c197524ad6fad16db43c75c36d7e28098607c
OscarCanongo <a01730443@itesm.mx>
2020-07-25 18:09:46 -0500

    Lista de profesores

--- baseConocimiento.pl
+++ baseConocimiento.pl
@@ -1,3 +1,4 @@
+%profesoresITC
 profesor("Dan Perez").
 profesor("Alberto Oliart").
 profesor("Claudia Perez Lezama").
@@ -17,3 +18,34 @@ profesor("Ana Lilia Reyes").
 profesor("Octavio Loyola").
 profesor("Alba Adriana Romero").
 profesor("Mario Farias").
+
+%profesoresMA
+profesor("Nicolas Amado Moranchel").
+profesor("Maria Montserrat Juarez").
+profesor("Gibran Sayeg Sanchez").
+profesor("Ivan Martinez").
+profesor("Juan Pablo Trevino").
+profesor("Julio Ramirez").
+profesor("Aida Ignacia Salazar").
+profesor("Mariana Olivares").
+profesor("Orlando Epifanio Gonzalez").
+profesor("Moises Elias Araiza").
+
+%profesoresNTCyNMA
+profesor("Humbelina Bolanos").
+profesor("Myriam Nieva").
+profesor("Luis Ricardo Fernandez").
+profesor("Flor del Carmen Paleta").
+profesor("Andrei Solorzano Perez").
+profesor("Javier Gomez").
+profesor("Iliana Barcenas").
+profesor("Laura Elena Gaona Lara").
+profesor("Clarisa Arlette Mendez").
+profesor("Claire Theste de Sagey").
+profesor("Ericka Bauchwitz").
+profesor("Guadalupe Castro").
+profesor("Antonio Mendez").
+profesor("Alma Karla Sandoval").
+profesor("Irving Juarez").
+profesor("Maria Concepcion Castillo").
+profesor("Cristian Consuelo Cruz").
