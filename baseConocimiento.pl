9661513237ffe0b4bd6e5b08ad6357dde3eda877
OscarCanongo <a01730443@itesm.mx>
2020-07-25 17:53:56 -0500

    First Commit

---
+++ baseConocimiento.pl
@@ -0,0 +1,19 @@
+profesor("Dan Perez").
+profesor("Alberto Oliart").
+profesor("Claudia Perez Lezama").
+profesor("Rosa Guadalupe").
+profesor("Luciano Garcia").
+profesor("Juan Manuel Calleros").
+profesor("David Augusto Cespedes").
+profesor("Adolfo Centeno").
+profesor("Ana Erendira Flores").
+profesor("Ivan Olmos").
+profesor("Gilberto").
+profesor("Veronica Rodriguez").
+profesor("Sandra Barajas").
+profesor("Jose Eduardo Ferrer").
+profesor("Jose Alberto Palomares").
+profesor("Ana Lilia Reyes").
+profesor("Octavio Loyola").
+profesor("Alba Adriana Romero").
+profesor("Mario Farias").
